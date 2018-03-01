752652c38c57b92a18f2080368fa8ee7839f7f87
Julio Marroquín <julio.marroquin.SubAVH@avianca.com>
2018-03-01 11:35:23 -0600

    p

--- server.js
+++ server.js
@@ -10,13 +10,12 @@ var express = require('express'),
 
 Object.assign=require('object-assign')
 
+var ip = require('ip');
+
 app.engine('html', require('ejs').renderFile);
 app.use(morgan('combined'))
 
-var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
-    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
-    mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
-    mongoURLLabel = "";
+
 
 
 
