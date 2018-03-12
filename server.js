8a77b90d87f83615a854bd48bf79876acffa9191
Julio Marroquín <julio.marroquin.SubAVH@avianca.com>
2018-03-12 15:19:52 -0600

    kjhj

--- server.js
+++ server.js
@@ -73,12 +73,10 @@ var initDb = function(callback) {
   });
 };
 
-app.get('/', function (reqs, res) {
-  var req = new XMLHttpRequest();
-  req.open('GET', document.location, false);
-  req.send(null);
-  var headers = req.getAllResponseHeaders().toLowerCase();
-  res.send(headers);
+app.get('/', function (req, res) {
+  var ips = getIp(req);
+ //your client ip
+  res.send(req.headers['x-forwarded-for']);
 
 
 
